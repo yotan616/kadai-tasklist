3661a3c56a138f0071a862b23b3bf1ab76b15f10
yotan616 <yotan616@gmail.com>
2017-12-10 08:44:47 +0000

    bootstrap design

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -5,12 +5,10 @@
     <h1>タスク新規作成ページ</h1>
     
 <div class="row">
-    <div class="col-xs-12">
-    <div class="col-sm-offset-2 col-sm-8 col-md-offset-2 col-md-8">
-    <div class="col-log-offset-3 col-log-6">
+    <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-md-offset-2 col-md-8 col-log-offset-3 col-log-6">
     {!! Form::model($task, ['route' => 'tasks.store']) !!}
     
-        <div class="form-group">
+                <div class="form-group">
                     {!! Form::label('status', 'ステータス:') !!}
                     {!! Form::text('status', null, ['class' => 'form-control']) !!}
                 </div>
@@ -23,8 +21,6 @@
                 {!! Form::submit('投稿', ['class' => 'btn btn-primary']) !!}
         
             {!! Form::close() !!}
-    </div>
-    </div>
     </div>
 </div>
 @endsection
--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -3,13 +3,12 @@
 @section('content')
 
     <h1>id: {{ $task->id }} のタスク編集ページ</h1>
-  <div class="row">
-        <div class="col-xs-12">
-        <div class="col-sm-offset-2 col-sm-8 col-md-offset-2 col-md-8">
-        <div class="col-log-offset-3 col-log-6">
+    <div class="row">
+        <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-md-offset-2 col-md-8 ol-log-offset-3 col-log-6">
+       
     {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
         
-        <div class="form-group">
+                <div class="form-group">
                     {!! Form::label('status', 'ステータス:') !!}
                     {!! Form::text('status', null, ['class' => 'form-control']) !!}
                 </div>
@@ -22,9 +21,7 @@
                 {!! Form::submit('更新', ['class' => 'btn btn-default']) !!}
         
             {!! Form::close() !!}
+        </div>
     </div>
-    </div>
-    </div>
-</div>
 
 @endsection
