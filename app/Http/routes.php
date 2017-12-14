f87cb0efbee1e36ebcc2ee7cafa535d3bbc688a8
yotan616 <yotan616@gmail.com>
2017-12-14 12:57:32 +0000

    user pages

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -40,3 +40,8 @@ Route::post('signup', 'Auth\AuthController@postRegister')->name('signup.post');
 Route::get('login', 'Auth\AuthController@getLogin')->name('login.get');
 Route::post('login', 'Auth\AuthController@postLogin')->name('login.post');
 Route::get('logout', 'Auth\AuthController@getLogout')->name('logout.get');
+
+// ログイン認証の確認
+Route::group(['middleware' => 'auth'], function () {
+    Route::resource('users', 'UsersController', ['only' => ['index', 'show']]);
+});
